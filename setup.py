41f6c79cfd02e21cab8fd8f3480d033b4abd05f7
Lee Brooks <lee@kagiso.io>
2015-07-10 12:53:48 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_django_auth',
-    version='0.2.0',
+    version='0.2.1',
     author='Kagiso Media',
     author_email='development@kagiso.io',
+    description='Kagiso Django AuthBackend',
     url='https://github.com/Kagiso-Future-Media/django_auth',
     packages=find_packages(),
     install_requires=[
