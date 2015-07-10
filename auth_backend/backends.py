ee0789dda32d58409fc81a9675ffcc2995fd5320
Lee Brooks <lee@kagiso.io>
2015-07-10 12:49:35 +0200

    Make all fields optional, so all scenarios work, assuming adequate args

--- auth_backend/backends.py
+++ auth_backend/backends.py
@@ -6,17 +6,15 @@ from .models import KagisoUser
 
 class KagisoBackend(ModelBackend):
 
-    # HACK: username is actually email, but Django passes in keyword args
-    # and expects username to exist
-    def authenticate(self, username, password, **kwargs):
-        # Django calls our backend with username='xyz', password='abc'
-        # e.g. credentials = {'username': 'Fred', 'password': 'open'}
-        # authenticate(**credentials), even though we set USERNAME_FIELD to
-        # 'email' in models.py.
-        # So we have to hack around it:
-        # https://github.com/django/django/blob/master/django/contrib/auth/__init__.py#L74
-
-        email = username
+    # Django calls our backend with username='xyz', password='abc'
+    # e.g. credentials = {'username': 'Fred', 'password': 'open'}
+    # authenticate(**credentials), even though we set USERNAME_FIELD to
+    # 'email' in models.py.
+    #
+    # Django AllAuth does this:
+    #  credentials = {'email': 'test@kagiso.io, 'password': 'open'}
+    def authenticate(self, email=None, username=None, password=None, **kwargs):
+        email = username if not email else email
         user = KagisoUser.objects.filter(email=email).first()
 
         if not user:
