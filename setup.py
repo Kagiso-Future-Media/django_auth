946f1c9ff0709f611ca8c71e6c52a666ec6a5423
Lee Brooks <lee@kagiso.io>
2015-11-04 10:26:22 +0200

    Add setting to install all files specified in manifest.in https://pythonhosted.org/setuptools/setuptools.html#new-and-changed-setup-keywords

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_django_auth',
-    version='3.0.2',
+    version='3.0.4',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Django AuthBackend',
     url='https://github.com/Kagiso-Future-Media/django_auth',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
             'jsonfield==1.0.3',
             'requests==2.8.1',
