46690e225c74516b539f1c42561a6cbb7c25ddfd
Kim <kim.petersend1@gmail.com>
2016-10-12 11:14:57 +0200

    bumped up version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_django_auth',
-    version='8.0.5',
+    version='8.1.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Django AuthBackend',
