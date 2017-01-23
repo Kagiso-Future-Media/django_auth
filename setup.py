48707b8bec9f68e3179b58f81c4818236e5f7c22
Michael Vorster <michaelvorster@Michaels-MacBook-Pro.local>
2017-01-23 17:06:25 +0200

    Removed erroneous and duplicated cell phone warning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_django_auth',
-    version='8.3.1',
+    version='8.3.2',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Django AuthBackend',
